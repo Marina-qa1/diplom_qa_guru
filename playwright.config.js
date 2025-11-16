157c492487dd1189f0f939ff67420d4e73245855
Marina-qa1 <marin.roginskaya@x5.ru>
2025-11-16 13:21:02 +0300

    update

--- playwright.config.js
+++ playwright.config.js
@@ -19,14 +19,17 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["line"],
-    ["html", { open: "never" }],
-    ["allure-playwright"],
+        ['html', { outputFolder: 'playwright-report' }],
+        ['allure-playwright', {
+            detail: true,
+            resultsDir: "allure-results",
+            suiteTitle: false
+             }]
   ] /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
